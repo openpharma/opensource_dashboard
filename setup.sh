adcbb729ad35de0de421463c52b5d32cb27a3a02
Mathieu cayssol <mathieucayssol@MacBook-Pro-de-Mathieu.local>
2022-06-07 14:01:57 +0200

    config.toml

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 [theme]\n\
-primaryColor="\#002171"\n\
-backgroundColor="\#F1F1F1"\n\
-secondaryBackgroundColor="\#FFFFFF"\n\
-textColor="\#262730"\n\
-font="sans serif"\n\
+primaryColor=\"\#002171\"\n\
+backgroundColor=\"\#F1F1F1\"\n\
+secondaryBackgroundColor=\"\#FFFFFF\"\n\
+textColor=\"\#262730\"\n\
+font=\"sans serif\"\n\
 " > ~/.streamlit/config.toml
