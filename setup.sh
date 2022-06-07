a92c809c29af030b653b7cd6304f694b449d8c84
Mathieu cayssol <mathieucayssol@MacBook-Pro-de-Mathieu.local>
2022-06-07 13:55:23 +0200

    aaa

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+[theme]\n\
+primaryColor="\#002171"\n\
+backgroundColor="\#F1F1F1"\n\
+secondaryBackgroundColor="\#FFFFFF"\n\
+textColor="\#262730"\n\
+font="sans serif"\n\
+" > ~/.streamlit/config.toml
