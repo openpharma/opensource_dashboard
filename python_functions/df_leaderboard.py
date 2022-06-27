5966295643b973aa0adbd8095546280975ed6ab6
MathieuCayssol <mathieu.cayssol@gmail.com>
2022-06-27 13:42:25 +0200

    rank activate

--- python_functions/df_leaderboard.py
+++ python_functions/df_leaderboard.py
@@ -11,7 +11,7 @@ def read_data_leaderboard(path: str) -> pd.DataFrame:
 def filter_df(df: pd.DataFrame):
     try:
         columns = ['avatar','author','Best_author', 'Best_replier','P_comments', 'P_reactions', 'FC_reactions', 'C_reactions', 'commits', 'contributed_to']
-        #df = df.sort_values(by='Best_author', ascending=False)[columns].reset_index(drop=True)
+        df = df.sort_values(by='Best_author', ascending=False)[columns].reset_index(drop=True)
         df=df[columns]
         df = df.rename(columns={'author': 'Username','Best_author': 'Best author on issues', 'Best_replier': 'Best replier on issues','P_comments': '#comments on my issues','P_reactions': '#reactions on my issues', 'FC_reactions': 'First comment','C_reactions': '#reactions on my comments', 'commits': '#commits', 'contributed_to': 'List repos'})
     except:
