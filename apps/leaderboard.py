34862d225867b7d564b778c82e3a2ecd939e9911
MathieuCayssol <mathieu.cayssol@gmail.com>
2022-06-30 16:43:02 +0200

    Remove old leaderboard

--- apps/leaderboard.py
+++ apps/leaderboard.py
@@ -9,10 +9,10 @@ def page_content():
 
     PATH = 'http://openpharma.s3-website.us-east-2.amazonaws.com/people_clean.csv'
     df = df_leaderboard.read_data_leaderboard(PATH)
-    df = df_leaderboard.filter_df(df)
+    #df = df_leaderboard.filter_df(df)
 
     st.title("Leaderboard")
 
-    st.markdown(df[:300].to_html(index=False, escape=False, formatters=dict(avatar=df_leaderboard.path_to_image_html)), unsafe_allow_html=True)
+    #st.markdown(df[:300].to_html(index=False, escape=False, formatters=dict(avatar=df_leaderboard.path_to_image_html)), unsafe_allow_html=True)
     
  
